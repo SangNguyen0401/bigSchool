cb093c023d77afd43ccb52e1dbf683fa3970e587
Administrator <Administrator@PC216>
2020-06-12 13:26:34 +0700

    Add project files.

---
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Requied]
+        public string Place { get; set; }
+        [Requied]
+        public string Date { get; set; }
+        [Requied]
+        public string Time { get; set; }
+        [Requied]
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
